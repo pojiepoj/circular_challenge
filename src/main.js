a1653ecfd83ed12d62fc34edab446f13f78c504e
pojie <po.johnpaul@gmail.com>
2021-09-09 21:23:22 +0800

    added Firebase
    added firebase configuration and firebase package

--- src/main.js
+++ src/main.js
@@ -2,5 +2,18 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import * as firebase from 'firebase/app'
+
+//  Firebase Configuration
+var firebaseConfig = {
+  apiKey: 'AIzaSyCuGHDwAT2WqmdSgxF4mU4CLp9izQkLa1U',
+  authDomain: 'pokemon-challange.firebaseapp.com',
+  projectId: 'pokemon-challange',
+  storageBucket: 'pokemon-challange.appspot.com',
+  messagingSenderId: '98622271907',
+  appId: '1:98622271907:web:92c27240933f1428acaa16'
+}
+
+firebase.initializeApp(firebaseConfig)
 
 createApp(App).use(store).use(router).mount('#app')
