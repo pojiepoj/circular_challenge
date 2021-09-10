112bf30c581b16659681daa85632937c13daeda7
pojie <po.johnpaul@gmail.com>
2021-09-10 22:57:24 +0800

    added firebase login function

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -7,40 +7,32 @@
       .form-field
         label.user(for='login-username')
           span.hidden Username
-        input#login-username.form-input(type='text' placeholder='Email' name="email" required='' v-model="form.email")
+        input#login-username.form-input(type='text' placeholder='Email' name="email" required='')
       .form-field
         label.lock(for='login-password')
           span.hidden Password
-        input#login-password.form-input(type='password' name='password' placeholder='Password' required='' v-model="form.password")
+        input#login-password.form-input(type='password' name='password' placeholder='Password' required='')
       .form-field
         input(type='submit' value='Log in')
 </template>
 
 <script>
-import * as firebase from 'firebase/app'
-
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import { useRouter } from 'vue-router'
 export default {
-  data () {
-    return {
-      form: {
-        email: '',
-        password: ''
-      },
-      error: null
+  setup () {
+    const auth = getAuth()
+    const router = useRouter()
+    const submit = async e => {
+      const { email, password } = e.target.elements
+      try {
+        await signInWithEmailAndPassword(auth, email.value, password.value)
+        router.push('/feed')
+      } catch (e) {
+        alert(e.message)
+      }
     }
-  },
-  methods: {
-    submit () {
-      firebase
-        .auth()
-        .signInWithEmailAndPassword(this.form.email, this.form.password)
-        .then(data => {
-          this.$router.replace({ name: 'Dashboard' })
-        })
-        .catch(err => {
-          this.error = err.message
-        })
-    }
+    return { submit }
   }
 }
 </script>
