6e851631fe01e2c6f7713251a9270e75c6cd5901
pojie <po.johnpaul@gmail.com>
2021-09-10 22:56:43 +0800

    New View
    added new vue for landing page after user has logged in

---
+++ src/views/Feed.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+h1 Hello world
+    button(@click='signOutUser')  Logout
+</template>
+
+<script>
+import { getAuth, signOut } from 'firebase/auth'
+import { useAuthState } from '../firebase'
+import { useRouter } from 'vue-router'
+export default {
+  name: 'Feed',
+  setup () {
+    const { user } = useAuthState()
+    const auth = getAuth()
+    const router = useRouter()
+    const signOutUser = async () => {
+      try {
+        await signOut(auth)
+        router.push('/')
+      } catch (e) {
+        alert(e.message)
+      }
+    }
+    return { user, signOutUser }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
