d5c5d016a4ecc1d864ca0f3ecd13106ebfc5d2fe
pojie <po.johnpaul@gmail.com>
2021-09-10 22:56:20 +0800

    updated router
    added checking for user state. if user not login, will go back to home or login page.

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,35 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from '../views/Home.vue'
+import { createRouter, createWebHistory } from 'vue-router'
+import { getUserState } from '../firebase'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: import('../views/Home.vue'),
+    meta: { requiresAuth: false }
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/feed',
+    name: 'Feed',
+    component: () => import('../views/Feed.vue'),
+    meta: { requiresAuth: true }
   }
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach(async (to, from, next) => {
+  const requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+  const requiresUnauth = to.matched.some(record => record.meta.requiresUnauth)
+
+  const isAuth = await getUserState()
+
+  if (requiresAuth && !isAuth) next('/')
+  else if (requiresUnauth && isAuth) next('/feed')
+  else next()
 })
 
 export default router
